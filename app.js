16defd0e1f55e723ebdbfec1dd96eaefb3f2c49b
Alawiye Muritala <99465163+emdes12@users.noreply.github.com>
2022-08-08 03:10:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -16,28 +16,51 @@ yearInp.value = "";
 cvvInp.value = "";
 
 function submitCard() {
-  nameDisp.textContent = nameInp.value;
-  monthDisp.textContent = monthInp.value;
-  yearDisp.textContent = yearInp.value;
-  cvvDisp.textContent = cvvInp.value;
+  if (numInp.value === "") {
+    numInp.parentElement.innerHTML += `<p style="color: red;">field Can't be blank</p>`;
+  } else if (isNaN(numInp.value)) {
+    numInp.parentElement.innerHTML += `<p style="color: red;">Wrong format, numbers only</p>`;
+  } else if (nameInp.value === "") {
+    nameInp.parentElement.innerHTML += `<p style="color: red;">field can not be blank</p>`;
+  } else if (monthInp.value === "") {
+    monthInp.parentElement.parentElement.innerHTML += `<p style="color: red;">field can't be blank</p>`;
+  } else if (yearInp.value === "") {
+    yearInp.parentElement.parentElement.innerHTML += `<p style="color: red;">field can't be blank</p>`;
+  } else if (cvvInp.value === "") {
+    cvvInp.parentElement.parentElement.innerHTML += `<p style="color: red;">field can't be blank</p>`;
+  } else {
+    nameDisp.textContent = nameInp.value;
+    monthDisp.textContent = monthInp.value;
+    yearDisp.textContent = yearInp.value;
+    cvvDisp.textContent = cvvInp.value;
 
-  let cardNumber = [];
+    let cardNumber = [];
 
-  for (let i = 0; i < numInp.value.length; i++) {
-    if (i === 3) {
-      cardNumber[i] = numInp.value[i] + " ";
-    } else if (i === 7) {
-      cardNumber[i] = numInp.value[i] + " ";
-    } else if (i === 11) {
-      cardNumber[i] = numInp.value[i] + " ";
-    } else {
-      cardNumber[i] = numInp.value[i];
+    for (let i = 0; i < numInp.value.length; i++) {
+      if (i === 3) {
+        cardNumber[i] = numInp.value[i] + " ";
+      } else if (i === 7) {
+        cardNumber[i] = numInp.value[i] + " ";
+      } else if (i === 11) {
+        cardNumber[i] = numInp.value[i] + " ";
+      } else {
+        cardNumber[i] = numInp.value[i];
+      }
     }
+    numDisp.textContent = cardNumber.join("");
+    nameInp.value = "";
+    numInp.value = "";
+    monthInp.value = "";
+    yearInp.value = "";
+    cvvInp.value = "";
+
+    // <!-- Completed state start -->
+
+    document.querySelector("section").innerHTML = `<div class="completed-state">
+    <div class="confirm-cirlce">&#10003</div>
+    <span> Thank you!</span>
+    <p>We've added your card details.</p>
+    <button onclick="location.reload()">Continue</button>
+    </div>`;
   }
-  numDisp.textContent = cardNumber.join("");
-  nameInp.value = "";
-  numInp.value = "";
-  monthInp.value = "";
-  yearInp.value = "";
-  cvvInp.value = "";
 }
